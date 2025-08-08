9d06e3cf3997beb5fe8a0c4236afcf4eb79c83aa
Elizabeth Saka <esaka28@andover.edu>
2025-08-08 11:57:55 -0400

    The app works now and you can delete birthdays if you get sum wrong

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -1,61 +1,63 @@
+//ContentView.swift
+// Birthdays
 //
-//  ContentView.swift
-//  Birthdays
+// Created by Scholar on 8/8/25.
 //
-//  Created by Scholar on 8/8/25.
-//
-
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
-    @Query private var friends: [Friend]
-    @Environment(\.modelContext) private var context
-    
-        @State private var newName = ""
-        @State private var newBirthday = Date.now
-    
-    
-    
-    
-    
-        var body: some View {
-            NavigationStack{
-                List(friends, id: \.name) {friend in
-                    HStack {
-                        Text(friend.name)
-                        Spacer()
-                        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
-                    }// end of HStack
-                }//end of list (line 18)
-                .navigationTitle("Birthdays")
-                .safeAreaInset(edge: .bottom){
-                    VStack(alignment: .center, spacing: 20 ){
-                        Text("New Birthday")
-                            .font(.headline)
-                        
-                        DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
-                            TextField("Name", text: $newName)
-                                .textFieldStyle(.roundedBorder)
-                        }//end of Date Picker
-                        
-                        Button("Save") {
-                            let newFriend = Friend(name: newName, birthday: newBirthday)
-                            context.insert(newFriend)
-                            newName = ""
-                            newBirthday = .now
-                            
-                        }//end of button
-                        .bold()
-                    }//end of VStack
-                    .padding()
-                    .background(.bar)
-                }//safeAreaInset
-            } // end of NStack
-        } // end of some view
-    }//end of content view
-
-#Preview {
-    ContentView()
-        .modelContainer(for:Friend.self, inMemory:true)
+  @Query private var friends: [Friend]
+  @Environment(\.modelContext) private var context
+  @State private var newName = ""
+  @State private var newBirthday = Date.now
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach(friends) { friend in
+          HStack {
+            Text(friend.name)
+            Spacer()
+            Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+          }
+        }
+        .onDelete(perform: deleteFriend)
+      }
+       
+       
+       
+       
+      .navigationTitle("Birthdays")
+      .safeAreaInset(edge: .bottom) {
+        VStack(alignment: .center, spacing:20) {
+          Text("New Birthday")
+            .font(.headline)
+          DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+            TextField("Name", text: $newName)
+              .textFieldStyle(.roundedBorder)
+          }
+          Button("Save") {
+            let newFriend = Friend(name: newName, birthday: newBirthday)
+            context.insert(newFriend)
+            newName = ""
+            newBirthday = .now
+          }
+          .bold ()
+        }
+        .padding()
+        .background(.bar)
+      }
+    }
+  }
+  func deleteFriend(at offsets: IndexSet) {
+    for index in offsets {
+      let friendToDelete = friends[index]
+      context.delete(friendToDelete)
+    }
+  }
 }
+  #Preview {
+    ContentView()
+      .modelContainer(for: Friend.self, inMemory: true)
+  }
+
+ 
