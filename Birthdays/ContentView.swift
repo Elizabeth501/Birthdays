9fa6c46700cf4036e511325a9684711f61138f89
Elizabeth Saka <esaka28@andover.edu>
2025-08-08 10:31:55 -0400

    Now able to see friends + birthday but can't change anything on the content view

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var friends: [Friend] = [
+        Friend (name: "Madison", birthday: .now),
+        Friend (name: "Kelechi", birthday: .now),
+    ]
+        @State private var newName = ""
+        @State private var newBirthday = Date.now
+    
+    
+    
+        var body: some View {
+            NavigationStack{
+                List(friends, id: \.name) {friend in
+                    HStack {
+                        Text(friend.name)
+                        Spacer()
+                        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                    }// end of HStack
+                }//end of list (line 18)
+                .navigationTitle("Birthdays")
+                .safeAreaInset(edge: .bottom){
+                }//safeAreaInset
+            } // end of NStack
+        } // end of some view
+    }//end of content view
 
 #Preview {
     ContentView()
